10a180712cea2d734965b2701ea829f318e5d2a9
thomas <thomas.pluess@mesch.ch>
2017-03-07 14:02:44 +0100

    make sure the forms dashboard page redirects to the first child on sitemap

--- controllers/single_page/dashboard/forms.php
+++ controllers/single_page/dashboard/forms.php
@@ -1,12 +1,20 @@
 <?php
 namespace Concrete\Package\AttributeForms\Controller\SinglePage\Dashboard;
 
-use Concrete\Core\Page\Controller\PageController;
+use Concrete\Core\Page\Controller\PageController,
+    Page,
+    URL;
 
 class Forms extends PageController
 {
     public function on_start()
     {
-        $this->redirect($this->action('types'));
+        $c = Page::getCurrentPage();
+        $children = $c->getCollectionChildrenArray();
+        if (!empty($children) && isset($children[0])) {
+            $this->redirect(URL::to(Page::getByID($children[0])));
+        } else {
+            $this->redirect($this->action('types'));
+        }
     }
 }
